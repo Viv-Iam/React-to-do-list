d0e02974243023ca9ea324a3e47a2cfcf5e28595
Vivi-Iam <vivian.opondoh@gmail.com>
2019-04-14 22:09:34 +0300

    Remove checkboxes into own component and import => add ToDoItem

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ToDoList from './components/ToDoItem'
 
-function App() {
-  return (
-    <div>
-    <input type="checkbox"/> <p className='p1'>I have a bike</p>
-    <input type="checkbox"/> <p className='p2'>I have a car</p>
-    <input type="checkbox"/> <p className='p3'>I have a boat</p>
-    </div>
-  )
-}
+// const styles = {
+// 	fontSize: 20,
+// 	backgroundColor: "black",
+// 	}
+// const date = new Date( )
+// const hours = date.getHours( )
+// let time
+
+// if(hours < 12) {
+// 	time = "morning"
+// 	styles.color = "green"
+// } else if(hours > 12 && hours < 17) {
+// 	time = "afternoon"
+// 	styles.color = "yellow"
+// } else {
+// 	time = "night"
+// 	styles.color = "teal"
+// }
+
+const App = ( ) => (
+	<div className="todo-list"> 
+    	{/* <h1 style={styles}> Good {time}, date is {`${date}`} </h1>
+	  	<h1 style={{color:"yellow"}}> I am enjoying react </h1> */}
+		< ToDoList />
+		< ToDoList />
+		< ToDoList />
+		< ToDoList />
+  </div>
+)
 
 export default App;
